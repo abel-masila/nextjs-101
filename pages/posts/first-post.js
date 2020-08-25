08355a3fd6e3efa1a5f9f2a7c4b596b04690a2f4
Abel Masila <abelmasila@gmail.com>
2020-08-26 00:45:49 +0300

    one more page

---
+++ pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+export default () => {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+  );
+};
